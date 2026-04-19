0bcf40a63349dbe90fec60c6186fefdf6a9fb850
雫凪 <iitaka.y.aa@m.titech.ac.jp>
2026-04-20 01:35:10 +0900

    Remove shebang for cleaner .bashrc integration

--- op.sh
+++ op.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
-
+# wsl-op: Open files/URLs with a spinner animation
 op() {
-    # Windows側で実行
     (explorer.exe "${@:-.}" > /dev/null 2>&1 &)
-
-    # アニメーション設定
     local spinner=('⢿' '⣻' '⣽' '⣾' '⣷' '⣯' '⣟' '⡿')
-    tput civis # カーソル非表示
-    
+    tput civis
     echo -ne "Now Loading...  "
     for i in {0..19}; do
         echo -ne "\b${spinner[i % 8]}"
         sleep 0.1
     done
     echo -e "\b Done!"
-    
-    tput cnorm # カーソル再表示
+    tput cnorm
 }
